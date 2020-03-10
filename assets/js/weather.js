800c35f60ea3f5fc0e706932bf13b1050fa40a8d
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-10 11:34:11 +0000

    changed back config after tested api console logging

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -7,7 +7,15 @@ var temp = document.querySelector('.temp');
 button.addEventListener('click',function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=c599e8cf88222b89d3d7fce3b12be2a1')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    nameValue = data['name'];
+    descValue = data['weather']['description'];
+    tempValue = data['main']['temp'];
+
+    name.innerHTML =nameValue;
+    desc.innerHTML =descValue;
+    temp.innerHTML =tempValue;
+    })
 
 .catch(err => alert("Please enter a valid city"))
 
