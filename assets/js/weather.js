ce1160529773968da3d5b75f2ca439c7050b5ce6
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-10 11:25:40 +0000

    add console log to identify error

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -4,18 +4,10 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
-button.addEventListener('click', function(){
+button.addEventListener('click',function(){
 fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=c599e8cf88222b89d3d7fce3b12be2a1')
 .then(response => response.json())
-.then(data => {
-    nameValue = data['name'];
-    descValue = data['weather']['description'];
-    tempValue = data['main']['temp'];
-
-    name.innerHTML =nameValue;
-    desc.innerHTML =descValue;
-    temp.innerHTML =tempValue;
-    })
+.then(data => console.log(data))
 
 .catch(err => alert("Please enter a valid city"))
 
