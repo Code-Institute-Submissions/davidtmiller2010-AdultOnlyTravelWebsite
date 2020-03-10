dfa0fc8454321c6035a785721c76feea5cbe047b
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-10 11:06:53 +0000

    Added a weather API call

---
+++ assets/js/weather.js
@@ -0,0 +1,22 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=c599e8cf88222b89d3d7fce3b12be2a1')
+.then(response => response.json())
+.then(data => {
+    nameValue = data['name'];
+    descValue = data['weather']['description'];
+    tempValue = data['main']['temp'];
+
+    name.innerHTML =nameValue;
+    desc.innerHTML =descValue;
+    temp.innerHTML =tempValue;
+    })
+
+.catch(err => alert("Please enter a valid city"))
+
+})
