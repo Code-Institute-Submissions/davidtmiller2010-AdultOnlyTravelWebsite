6c41fd05926fecd348423021eeb0da6eaa0b4f3a
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-13 16:13:21 +0000

    checking console output2

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -17,7 +17,6 @@ var temp3 = document.querySelector('.temp3');
 button.addEventListener('click',function(){
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=c599e8cf88222b89d3d7fce3b12be2a1&units=metric')
 .then(response => response.json())
-.then(data => console.log(data))
 .then(data => {
     nameValue = data['city']['name'];
     dateValue = data[3]['sys']['dt_txt'];
