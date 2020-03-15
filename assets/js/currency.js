3f19ca189fa567ed4421f05cf5d539fcf2ed6e5f
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-15 19:22:10 +0000

    changed API1

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -1,12 +1,44 @@
-var button = document.querySelector('.button1')
-var fromAmount = document.querySelector('amount');
-var exchange = document.querySelector('.exchange');
-var from = document.querySelector('.from');
-var to = document.querySelector('.to');
+var https = require('https');
 
-button.addEventListener('click',function(){
-fetch('https://free.currconv.com/api/v7/convert?q='+ from + '_' + to + '&compact=ultra&apiKey=7fcad69c743cb87e4a7a')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => alert("Please Try Again"))
-})
+function convertCurrency(amount, fromCurrency, toCurrency, cb) {
+  var apiKey = '7fcad69c743cb87e4a7ayour-api-key-here';
+
+  amount = document.querySelector('amount');
+  fromCurrency = document.querySelector('from');
+  toCurrency = document.querySelector('to');
+  var query = fromCurrency + '_' + toCurrency;
+
+  var url = 'https://api.currconv.com/api/v7/convert?q='
+            + query + '&compact=ultra&apiKey=' + apiKey;
+
+  https.get(url, function(res){
+      var body = '';
+
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+
+      res.on('end', function(){
+          try {
+            var jsonObj = JSON.parse(body);
+            
+            var val = jsonObj[query];
+            if (val) {
+              var total = val * amount;
+              cb(null, Math.round(total * 100) / 100);
+              exchange.innerHTML=cb;
+            } else {
+              var err = new Error("Value not found for " + query);
+              console.log(err);
+              cb(err);
+            }
+          } catch(e) {
+            console.log("Parse error: ", e);
+            cb(e);
+          }
+      });
+  }).on('error', function(e){
+        console.log("Got an error: ", e);
+        cb(e);
+  });
+}
