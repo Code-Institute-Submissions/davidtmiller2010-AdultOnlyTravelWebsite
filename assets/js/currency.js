4a48e0780180bff7b7cd84e0d088e0c749bd7752
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-15 18:31:06 +0000

    update currency js console data3

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -5,7 +5,7 @@ var from = document.querySelector('.from');
 var to = document.querySelector('.to');
 
 button.addEventListener('click',function(){
-fetch('http://data.fixer.io/api/latest?symbols=' + from + ',' + to + '&access_key=a4d6fa9ce8bfb156d0704dc14a5ec25c')
+fetch('https://data.fixer.io/api/latest?symbols=' + from + ',' + to + '&access_key=a4d6fa9ce8bfb156d0704dc14a5ec25c')
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(err => alert("Please Try Again"))
