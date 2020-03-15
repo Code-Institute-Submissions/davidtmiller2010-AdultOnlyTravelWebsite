5806f7886f6e71b24b8412fdb050d3bdee96a5f1
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-15 20:43:43 +0000

    test api4

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -7,7 +7,9 @@ var to = document.querySelector('.to');
 button.addEventListener('click',function(){
     fetch('https:///openexchangerates.org/api/latest.json?app_id=96957396eceb4037b457215886939270')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => 
+console.log(data.rates))
+    
 .catch(err => alert("Please Try Again"))
 
 })
