466e30d32982e5118cc04c4f5ebe1bf74c159b89
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-15 20:17:41 +0000

    test api3

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -5,7 +5,7 @@ var from = document.querySelector('.from');
 var to = document.querySelector('.to');
 
 button.addEventListener('click',function(){
-    fetch('https://openexchangerates.org/api/convert/'+ fromAmount +'/' + from + '/' + to + '?app_id=96957396eceb4037b457215886939270')
+    fetch('https:///openexchangerates.org/api/latest.json?app_id=96957396eceb4037b457215886939270')
     .then(response => response.json())
     .then(data => console.log(data))
 .catch(err => alert("Please Try Again"))
