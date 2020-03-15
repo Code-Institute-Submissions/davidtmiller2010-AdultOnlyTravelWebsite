91a83267f671cbcf61481a718ea668a4a5a6d6cb
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-15 18:36:53 +0000

    changing back to http

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -5,7 +5,7 @@ var from = document.querySelector('.from');
 var to = document.querySelector('.to');
 
 button.addEventListener('click',function(){
-fetch('https://data.fixer.io/api/latest?symbols=' + from + ',' + to + '&access_key=a4d6fa9ce8bfb156d0704dc14a5ec25c')
+fetch('http://data.fixer.io/api/latest?symbols=' + from + ',' + to + '&access_key=a4d6fa9ce8bfb156d0704dc14a5ec25c')
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(err => alert("Please Try Again"))
