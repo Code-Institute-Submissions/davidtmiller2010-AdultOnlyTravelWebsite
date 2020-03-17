c4babbfe86d4e653bef2c2b5ed167964ed082b21
davidtmiller2010 <davidtmiller2010@gmail.com>
2020-03-17 20:43:44 +0000

    updated currency converter to work

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,25 +1,24 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "contact_email", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "from_destination": contactForm.destination.value,
-        "from_noTravelling": contactForm.quantity.value,
-        "from_fromDate": contactForm.startdate.value,
-        "from_toDate": contactForm.enddate.value,
-        "from_info": contactForm.holidaywishlist.value,
+  emailjs
+    .send("gmail", "contact_email", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      from_destination: contactForm.destination.value,
+      from_noTravelling: contactForm.quantity.value,
+      from_fromDate: contactForm.startdate.value,
+      from_toDate: contactForm.enddate.value,
+      from_info: contactForm.holidaywishlist.value
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            alert("The form was sent");
-            document.getElementById("Form").reset();
-        },
-        function(error) {
-            console.log("FAILED", error);
-            alert("The form was not sent");
-        }
+      function(response) {
+        console.log("SUCCESS", response);
+        alert("The form was sent");
+        document.getElementById("Form").reset();
+      },
+      function(error) {
+        console.log("FAILED", error);
+        alert("The form was not sent");
+      }
     );
-    return false;  // To block from loading a new page
+  return false; // To block from loading a new page
 }
-
-
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,35 +1,37 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
-var desc2 = document.querySelector('.desc2');
-var temp2 = document.querySelector('.temp2');
-var desc3 = document.querySelector('.desc3');
-var temp3 = document.querySelector('.temp3');
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputValue");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+var desc2 = document.querySelector(".desc2");
+var temp2 = document.querySelector(".temp2");
+var desc3 = document.querySelector(".desc3");
+var temp3 = document.querySelector(".temp3");
 
-button.addEventListener('click',function(){
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=c599e8cf88222b89d3d7fce3b12be2a1&units=metric')
-.then(response => response.json())
-.then(data => {
-    descValue = data['list'][3]['weather'][0]['description'];
-    tempValue = data['list'][3]['main']['temp'];
+button.addEventListener("click", function() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      inputValue.value +
+      "&appid=c599e8cf88222b89d3d7fce3b12be2a1&units=metric"
+  )
+    .then(response => response.json())
+    .then(data => {
+      descValue = data["list"][3]["weather"][0]["description"];
+      tempValue = data["list"][3]["main"]["temp"];
 
-    desc2Value = data['list'][11]['weather'][0]['description'];
-    temp2Value = data['list'][11]['main']['temp'];
+      desc2Value = data["list"][11]["weather"][0]["description"];
+      temp2Value = data["list"][11]["main"]["temp"];
 
-    desc3Value = data['list'][19]['weather'][0]['description'];
-    temp3Value = data['list'][19]['main']['temp'];
+      desc3Value = data["list"][19]["weather"][0]["description"];
+      temp3Value = data["list"][19]["main"]["temp"];
 
-    desc.innerHTML =descValue;
-    temp.innerHTML =tempValue;
+      desc.innerHTML = descValue;
+      temp.innerHTML = tempValue;
 
-    desc2.innerHTML =desc2Value;
-    temp2.innerHTML =temp2Value;
-    
-    desc3.innerHTML =desc3Value;
-    temp3.innerHTML =temp3Value;
+      desc2.innerHTML = desc2Value;
+      temp2.innerHTML = temp2Value;
 
+      desc3.innerHTML = desc3Value;
+      temp3.innerHTML = temp3Value;
     })
-.catch(err => alert("Please enter a valid city"))
-
-})
+    .catch(err => alert("Please enter a valid city"));
+});
